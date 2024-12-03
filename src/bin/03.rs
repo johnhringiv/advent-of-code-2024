8236b439304fc773a0c4f6620e8687430830cf5b
John H. Ring IV <johnhringiv@gmail.com>
2024-12-03 13:24:04 -0500

    Avoid second regex

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,9 +1,9 @@
 use adv_code_2024::*;
 use code_timing_macros::time_snippet;
 use const_format::concatcp;
+use regex::Regex;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use regex::{Regex};
 
 const DAY: &str = "03";
 const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
@@ -21,28 +21,24 @@ fn part1<R: BufRead>(reader: R) -> usize {
             let n2 = m2.parse::<usize>().unwrap();
             result += n1 * n2;
         }
-
     }
     result
 }
 
 fn part2<R: BufRead>(reader: R) -> usize {
-    let re = Regex::new(r"mul\(\d+,\d+\)|(do\(\))|(don't\(\))").unwrap();
-    let mul_re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let re = Regex::new(r"mul\((\d+),(\d+)\)|(do\(\))|(don't\(\))").unwrap();
     let mut result = 0;
     let mut state_do = true;
     for line in reader.lines() {
         let line = line.unwrap();
-        let matches: Vec<_> = re.find_iter(&line).map(|m| m.as_str()).collect();
-        for ins in matches {
-            match ins {
+        for ins in re.captures_iter(&line) {
+            match ins.get(0).unwrap().as_str() {
                 "do()" => state_do = true,
                 "don't()" => state_do = false,
                 _ => {
                     if state_do {
-                        let (_, [m1, m2]) = mul_re.captures(ins).unwrap().extract();
-                        let n1 = m1.parse::<usize>().unwrap();
-                        let n2 = m2.parse::<usize>().unwrap();
+                        let n1 = ins.get(1).unwrap().as_str().parse::<usize>().unwrap();
+                        let n2 = ins.get(2).unwrap().as_str().parse::<usize>().unwrap();
                         result += n1 * n2;
                     }
                 }
@@ -55,9 +51,7 @@ fn part2<R: BufRead>(reader: R) -> usize {
 fn main() {
     start_day(DAY);
 
-    println!("=== Load Data ===");
     let input_file = BufReader::new(File::open(INPUT_FILE).unwrap());
-
     println!("\n=== Part 1 ===");
     assert_eq!(161, part1(BufReader::new(TEST.as_bytes())));
     let result = time_snippet!(part1(input_file));
